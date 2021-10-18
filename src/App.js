8136d8615f72073744ed8cd16affa5e511742983
sonaliSL <sonali@cambridge-intelligence.co.uk>
2021-10-18 12:34:34 +0100

    inserted skeleton app for start

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
 
 import './App.css';
 import React from 'react';
-import { hogwartsStudentRegistry } from './data';
-import Register from './Register';
-import StudentForm from './StudentForm';
-import Search from './Search';
 
 class App extends React.Component {
   constructor(props) {
     super();
-    this.state = {
-      students: [],
-      selectedStudent: [],
-    }
   }
 
-  componentDidMount = () => {
-    this.updateStudentRecord(hogwartsStudentRegistry);
-  }
-
-  updateStudentRecord = (students) => {
-    const mapStudents = students.map((student, i) => ({
-      ...student, id: i
-    }));
-    this.setState({students: mapStudents});
-  }
-
-  handleInputChange = (e) => {
-    this.setState({[e.target.id]: e.target.value})
-  }
-
-  displayStudent = (student) => {
-    this.setState({selectedStudent: student})
-  }
-
-  render() { 
-    const {students, selectedStudent} = this.state;
+  render() {
     return (
-    <div className="app-wrapper">
-      <div id="app-lhs-container">
-        <Search students={students} displayStudent={this.displayStudent} updateStudentRecord={this.updateStudentRecord}/>
-        <StudentForm updateStudents={this.updateStudentRecord} students={students}/>
+      <div className="app-wrapper">
+        <h1>Insert code here!</h1>
       </div>
-      <div id="app-rhs-container">
-        <Register students={students} selectedStudent={selectedStudent} updateStudents={this.updateStudentRecord} />
-      </div>
-    </div>
-  );
+    )
   }
 }
 
